ae5c11f85e50bf21bfa17848c08d4de95d3fba60
Eric Zhang <3ric@berkeley.edu>
2022-11-29 19:13:18 -0800

    fix bug w/ null notespawner

--- Assets/Scripts/NoteBehavior.cs
+++ Assets/Scripts/NoteBehavior.cs
@@ -28,6 +28,7 @@ public class NoteBehavior : MonoBehaviour
     }
 
     private int getTimingBucket() {
+        Debug.Log(noteSpawner);
         float diff = Math.Abs(noteSpawner.getSongPos() - timeStamp);
         int bucketIndex = 0;
         for (;bucketIndex < Timings.timingBuckets.Length; bucketIndex++) {
